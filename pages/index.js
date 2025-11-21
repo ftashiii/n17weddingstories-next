540b54ec7d1ef910b52807eb21147e4fc1924de3
ftashiii <ftashiii4@gmail.com>
2025-11-22 02:07:45 +0530

    Remove globals.css import from index.js
    Removed import statement for globals.css.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
-import "../styles/globals.css";
+
  // small helper styles we put in globals.css
 
 export default function Home() {
