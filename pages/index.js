288239af50088dd853e8031d620facefcdf60d3a
ftashiii <ftashiii4@gmail.com>
2025-11-25 20:04:16 +0530

    Import global styles in index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 
+import styles from "../styles/globals.module.css";
 export default function Home() {
   const [openContact, setOpenContact] = useState(false);
 
